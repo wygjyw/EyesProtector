50d967c10b103d34f39dd46568594f0e62c2ca4a
wygjyw <wygjyw@gmail.com>
2013-04-26 14:58:35 +0800

    init the perference UI

---
+++ EyesProtector/PerferencesWnd.h
@@ -0,0 +1,13 @@
+//
+//  PerferencesWnd.h
+//  EyesProtector
+//
+//  Created by Wyg Jyw on 4/26/13.
+//  Copyright (c) 2013 wygjyw. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PerferencesWnd : NSWindowController
+
+@end
---
+++ EyesProtector/PerferencesWnd.m
@@ -0,0 +1,34 @@
+//
+//  PerferencesWnd.m
+//  EyesProtector
+//
+//  Created by Wyg Jyw on 4/26/13.
+//  Copyright (c) 2013 wygjyw. All rights reserved.
+//
+
+#import "PerferencesWnd.h"
+
+@interface PerferencesWnd ()
+
+@end
+
+@implementation PerferencesWnd
+
+- (id)initWithWindow:(NSWindow *)window
+{
+    self = [super initWithWindow:window];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)windowDidLoad
+{
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
