7f6e09757bdc6cfb82d2f4981bef60516d1748bb
wygjyw <wygjyw@gmail.com>
2013-04-27 14:38:12 +0800

    init full screen UI

---
+++ EyesProtector/EyesProtector/SSFullScreen.h
@@ -0,0 +1,13 @@
+//
+//  SSFullScreen.h
+//  EyesProtector
+//
+//  Created by Wyg Jyw on 4/27/13.
+//  Copyright (c) 2013 wygjyw. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SSFullScreen : NSWindowController
+
+@end
---
+++ EyesProtector/EyesProtector/SSFullScreen.m
@@ -0,0 +1,34 @@
+//
+//  SSFullScreen.m
+//  EyesProtector
+//
+//  Created by Wyg Jyw on 4/27/13.
+//  Copyright (c) 2013 wygjyw. All rights reserved.
+//
+
+#import "SSFullScreen.h"
+
+@interface SSFullScreen ()
+
+@end
+
+@implementation SSFullScreen
+
+- (id)initWithWindow:(NSWindow *)window
+{
+    self = [super initWithWindow:window];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)windowDidLoad
+{
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
